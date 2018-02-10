61d3b6adfc287a0d63dea64d20ffc94a402cd9ff
Ana Luisa Lamberto <alamber2@nd.edu>
2018-02-10 02:33:15 -0500

    homework03: activity 3

--- homework03/homework03/zipcode.sh
+++ homework03/homework03/zipcode.sh
@@ -15,10 +15,9 @@ EOF
 
 doSearch(){
   if [ "$1" = "0" ]; then
-    #find zipcode with city, replace everything before > with nothing, replace evreything after < with nothing
-    curl -s http://www.zipcodestogo.com/$STATE/ | grep -Po "/$CITY/.*>\d{5}<" | sed -e "s|/$CITY/.*>||" -e 's|<||' | sort | uniq
+    curl -sL http://www.zipcodestogo.com/$STATE/ | grep -b -1 ">$CITY<" | grep -Po ">\d{5}<" | grep -Po "\d{5}" | sort | uniq
   else #find 5 digit numbers surrounded by ><, replace >< with nothing
-    curl -s http://www.zipcodestogo.com/$STATE/ | grep -Po '>\d{5}<' | sed -e 's|>||' -e 's|<||' | sort | uniq
+    curl -sL http://www.zipcodestogo.com/$STATE/ | grep -Po ">\d{5}<" | grep -Po "\d{5}" | sort | uniq
   fi
 }
 
@@ -29,39 +28,21 @@ CHOICE=1 #controls what type of grep is called
 
 while [ $# -gt 0 ]; do
   case "$1" in
-    -h) #usage
+    -h)
       usage 0
       ;;
-    -c) #search by city
+    -c)
       CITY=$2
       CHOICE=0
       shift
-      if [[ $2 == [[:alpha:]]* ]]; then #if next argument is another word, add to city name
-        CITY="$CITY $2"
-        #echo $CITY | sed "s/\"//" #remove " "
-        shift
-      fi
       ;;
-    -s) #search by state
-      if [[ $2 == *New ]]; then
-        STATE="$2%20$3"
-        echo $STATE
-        shift
-      elif [[ $2 == *North ]]; then
-        STATE="$2%20$3"
-        shift
-      elif [[ $2 == *South ]]; then #check for two name states
-        STATE="$2%20$3" #change to %20 for URL
-        shift
-      #echo $STATE | sed "s/\"//" #remove " "
-      else #if state is one word
-        STATE=$2
-        #| sed -e "s/^\"//" -e "s/\"$//" #remove " "
-      fi
+    -s)
+      STATE=$2
+      STATE="$(sed s/' '/%20/ <<<$STATE)"
       shift
       ;;
-  esac
-  shift
+    esac
+    shift
 done
 
 doSearch $CHOICE
