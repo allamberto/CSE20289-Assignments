61532ad5ed9e901d90ebaac26f022337333c4fc5
Ana Luisa Lamberto <alamber2@nd.edu>
2018-04-06 21:23:39 -0400

    homework07: tree

--- homework07/tree.c
+++ homework07/tree.c
@@ -18,7 +18,13 @@ void	tree_walk_node(const Node *n, NodeFunc f, void *arg, char *encoding);
  * @return  Newly allocated and initialized Tree structure.
  */
 Tree *  tree_create() {
-    return NULL;
+  Tree *t = calloc(1, sizeof(Tree));
+  if(t){
+    t->root = NULL;
+    int64_t countsTemp[TREE_COUNTS_SIZE] = {0};
+    memcpy(t->counts, countsTemp, sizeof(countsTemp));
+  }
+    return t;
 }
 
 /**
@@ -27,7 +33,9 @@ Tree *  tree_create() {
  * @return  NULL.
  */
 Tree *  tree_delete(Tree *t) {
-    return NULL;
+  node_delete(t->root);
+  free(t);
+  return NULL;
 }
 
 /**
@@ -37,7 +45,19 @@ Tree *  tree_delete(Tree *t) {
  * @return  Whether or not the operation succeeded.
  */
 bool	tree_build(Tree *t, FILE *stream) {
+  tree_count(t, stream);
+  Queue *q = tree_queue(t);
+  if(!q)
     return false;
+  while(q->size > 1){
+    Node *node1 = queue_pop(q);
+    Node *node2 = queue_pop(q);
+    Node *newNode = node_create(NODE_NOT_SYMBOL, node1->count + node2->count, node1, node2);
+    queue_push(q, newNode);
+  }
+  t->root = queue_pop(q);
+  queue_delete(q);
+  return true;
 }
 
 /**
@@ -48,6 +68,8 @@ bool	tree_build(Tree *t, FILE *stream) {
  * @return  Whether or not the operation succeeded.
  */
 void	tree_walk(const Tree *t, NodeFunc f, void *arg) {
+  char encoding[BUFSIZ] = "";
+  tree_walk_node(t->root, f, arg, encoding);
 }
 
 /* Internal Tree Functions */
@@ -59,6 +81,11 @@ void	tree_walk(const Tree *t, NodeFunc f, void *arg) {
  * @return
  */
 void	tree_count(Tree *t, FILE *stream) {
+  char buffer[BUFSIZ];
+  while(fgets(buffer, BUFSIZ, stream)){
+    for(int i = 0; i < strlen(buffer); i++)
+      t->counts[(int)buffer[i]]++;
+  }
 }
 
 /**
@@ -67,7 +94,15 @@ void	tree_count(Tree *t, FILE *stream) {
  * @return  Priority Queue of Nodes corresponding to counts in Tree.
  */
 Queue *	tree_queue(Tree *t) {
-    return NULL;
+  Queue *q = queue_create(TREE_COUNTS_SIZE);
+  if(q){
+    for(int i = 0; i < TREE_COUNTS_SIZE; i++){
+      if(t->counts[i]){
+        queue_push(q, node_create(i, t->counts[i], NULL, NULL));
+      }
+    }
+  }
+  return q;
 }
 
 /**
@@ -78,6 +113,24 @@ Queue *	tree_queue(Tree *t) {
  * @param   encoding    Node encoding.
  */
 void	tree_walk_node(const Node *n, NodeFunc f, void *arg, char *encoding) {
+  if(!n)
+    return;
+  if(!n->left && !n->right){
+    if (strlen(encoding) == 0){
+      strcat(encoding, "0");
+    }
+    f(n, encoding, arg);
+  }
+  else{
+    if(n->left){
+      tree_walk_node(n->left, f, arg, strcat(encoding, "0"));
+      encoding[strlen(encoding) - 1] = '\0';
+    }
+    if(n->right){
+      tree_walk_node(n->right, f, arg, strcat(encoding, "1"));
+      encoding[strlen(encoding) - 1] = '\0';
+    }
+  }
 }
 
 /* vim: set sts=4 sw=4 ts=8 expandtab ft=c: */
