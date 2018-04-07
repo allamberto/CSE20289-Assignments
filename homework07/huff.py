989008c662c50e78c8ac21211f595c0a4487637b
Ana Luisa Lamberto <alamber2@nd.edu>
2018-04-06 23:45:54 -0400

    Python

--- homework07/huff.py
+++ homework07/huff.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python3
 
 import os
-import string
 import sys
+import yaml
 
-if __name__ == '__main__':
-    urlList = list()
-    response = requests.get('https://www.gutenberg.org/files/1342/1342-pdf.pdf?session_id=a798f059d3dc54813d3316522cb424020e3db481')
-    urlList.append(response)
-    response = requests.get('https://www.gutenberg.org/files/1342/1342-pdf.pdf?session_id=a798f059d3dc54813d3316522cb424020e3db481')
-    urlList.append(response)
-    book = response.json()
-    for line in book:
-        os.popen(huff)
+books = ['monte_cristo.txt', 'beowulf.txt', 'les_mis.txt', 'frankenstein.txt', 'iliad.txt']
+
+print("| {:20}| {:13}| {:13}| {:13}|".format("FILE", "OLD SIZE", "NEW SIZE", "RATIO"))
+print("|{:-<21}|{:-<14}|{:-<14}|{:-<14}|".format("-", "-", "-", "-"))
+
+for i in books:
+    nSize = 0
+
+    cmd = 'iconv -sc -f utf-8 -t iso-8859-1 ' + i + ' > curr.ascii'
+    os.system(cmd)
+
+    ostr = "stat -c '%s' curr.ascii"
+    oSize = os.path.getsize('curr.ascii')
+
+    #oSize = int((os.popen(ostr).read())) * 8
+
+    dstr = './huff -f json curr.ascii'
+
+    d = yaml.load(os.popen(dstr))
+
+    print(d)
+
+    for key in d:
+        nSize += len(d[key]['encoding']) * int(d[key]['count'])
+
+    ratio = (nSize / oSize) * 100
+
+    print("| {:20}| {:13}| {:13}| {:i>12}%|".format(i, oSize, nSize, '%.2f'% ratio))
