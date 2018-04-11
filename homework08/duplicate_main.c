13710cab3600a6b3f7c6327a6376817fc2972a2c
Ana Luisa Lamberto <alamber2@nd.edu>
2018-04-10 22:48:23 -0400

    homework08: starter code

---
+++ homework08/duplicate_main.c
@@ -0,0 +1,17 @@
+/* duplicate_main.c: Main Execution */
+
+#include "duplicate.h"
+
+#include <errno.h>
+#include <limits.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+/* Main Execution */
+
+int         main(int argc, char *argv[]) {
+}
+
+/* vim: set sts=4 sw=4 ts=8 expandtab ft=c: */
